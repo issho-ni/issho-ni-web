01634cf8ddef80242163e86f634734f41e7adb45
Jesse B. Hannah <jesse@jbhannah.net>
2020-04-06 14:40:24 -0700

    React

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+const app = document.createElement("div")
+document.body.prepend(app)
+
+ReactDOM.render(<div>Hello, World!</div>, app)
