6b275f081e78cf7408d4f8b6e242fd9f2cd74eaa
Jesse B. Hannah <jesse@jbhannah.net>
2020-04-15 13:52:48 -0700

    Add constants typings

---
+++ typings/constants.d.ts
@@ -0,0 +1,12 @@
+declare const FIREBASE_CONFIG: {
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+  measurementId: string
+}
+
+declare const FIREBASE_SIGN_IN_OPTIONS: string[]
